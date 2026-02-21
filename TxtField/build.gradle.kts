6005a5a77dba5ae485ae4821ff6601fd761e6e8a
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-21 17:59:52 +0200

    Tweak build.gradle

--- TxtField/build.gradle.kts
+++ TxtField/build.gradle.kts
@@ -7,9 +7,7 @@ plugins {
 android {
     namespace = "io.github.vankorno.txtfield"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(36)
     }
     
     defaultConfig {
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,9 +6,7 @@ plugins {
 android {
     namespace = "io.github.vankorno.txtfieldtest"
     compileSdk {
-        version = release(36) {
-            minorApiLevel = 1
-        }
+        version = release(36)
     }
     
     defaultConfig {
