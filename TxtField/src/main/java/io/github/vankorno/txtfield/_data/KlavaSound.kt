41e787614b2ea97a65c358db4420af8a094367a9
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-02-24 23:31:43 +0200

    ...

--- TxtField/src/main/java/io/github/vankorno/txtfield/_data/KlavaSound.kt
+++ TxtField/src/main/java/io/github/vankorno/txtfield/_data/KlavaSound.kt
@@ -1,5 +1,11 @@
 package io.github.vankorno.txtfield._data
 
-enum class KlavaSound {
-    
+import io.github.vankorno.txtfield.R
+
+enum class KlavaSound(                                                          val file: Int
+) {
+    Error(R.raw.error_easy),
+    Click(R.raw.keyboard_normal_click),
+    ClickLarge(R.raw.keyboard_large_key_click),
+    ClickUnique(R.raw.keyboard_key_press_unique), // TODO Not used yet
 }
