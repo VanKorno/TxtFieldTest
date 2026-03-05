a2ec70e290814876871d390a3b6e06c028806692
VanKorno <71512024+VanKorno@users.noreply.github.com>
2026-03-05 15:19:28 +0200

    update

--- TxtField/src/main/java/io/github/vankorno/txtfield/_vm/VmTycjalkaHolder.kt
+++ TxtField/src/main/java/io/github/vankorno/txtfield/_vm/VmTycjalkaHolder.kt
@@ -1,25 +1,7 @@
 package io.github.vankorno.txtfield._vm
 
-import android.app.Application
-import androidx.activity.ComponentActivity
-import androidx.lifecycle.SavedStateViewModelFactory
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
+import com.vankorno.vankornocompose.vm.VmHolder
 
-object VmTycjalkaHolder {
-    private lateinit var provider: ViewModelProvider
-    
+object VmTycjalkaHolder : VmHolder() {
     val vmTycjalka by lazy { get<VmTycjalka>() }
-    
-    private inline fun <reified T : ViewModel> get(): T = provider[T::class.java]
-    
-    
-    fun init(                                                           activity: ComponentActivity,
-                                                                     application: Application,
-    ) {
-        provider = ViewModelProvider(
-            owner = activity,
-            factory = SavedStateViewModelFactory(application, activity)
-        )
-    }
 }
